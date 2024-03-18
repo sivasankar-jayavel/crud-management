af8bb8c04b119e96b2cf538b172cf45881b50193
sivasankar-jayavel <sivasankar6104@gmail.com>
2024-03-18 18:10:50 +0530

    Alignment

--- main.js
+++ main.js
@@ -9,7 +9,10 @@ const Port = process.env.PORT || 3000;
 
 // DB Connection
 const connectDB = ()=>{
-    mongoose.connect(process.env.DB_URI).then(con=>{
+    mongoose.connect(process.env.DB_URI,{
+        useNewUrlParser : true,
+        useUnifiedTopology:true
+    }).then(con=>{
         console.log(`MongoDB is connected to the Host : ${con.connection.host}`)
     });
 }
